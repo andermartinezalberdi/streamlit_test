17ab291736ea0fec8e121642ae8ae98e37a6262f
ander <ander.martinez.alberdi@gmail.com>
2025-12-02 13:32:57 -0600

    Agregar simulador de tirada de moneda

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import random
 
-st.title("Hola desde Streamlit en Render")
-st.write("Si ves este texto, la app está funcionando correctamente.")
+st.title("Simulador de tirada de moneda")
+
+st.write("Haz clic en el botón para tirar la moneda:")
+
+if st.button("Tirar moneda"):
+    resultado = random.choice(["Cara", "Cruz"])
+    st.write(f"Resultado: **{resultado}**")
+else:
+    st.write("Aún no has tirado la moneda.")
